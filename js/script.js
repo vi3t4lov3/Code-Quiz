d534280dd4e6a84b1bd1be794dd9b2922b5b2beb
Tu Nguyen <tuinfor@ymail.com>
2022-03-31 21:45:43 -0400

    display the score but not save yet

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 var quiztimeEl = document.querySelector(".quiztime");
+var headercontainerEl = document.querySelector(".header-container");
 var startButton = document.querySelector(".start-button");
 var startagainButton = document.querySelector(".start-again-button");
 var correct = document.querySelector(".correct");
 var questionEl = document.querySelector(".question");
 var answerEl = document.querySelector(".answer");
-var viewscoreEl = document.querySelector(".score");
+var viewscoresEl = document.querySelector(".view-scores");
+var displayresultsEl = document.querySelector(".quiz-result")
+var quizcontainerEl = document.querySelector(".quiz-container");
 
 var timer;
 var timeCount;
-
+var questionCount = 0;
+var score = 0 ;
 var questionList = [
     {
         question: `How do you write "Hello World" in an alert box?`,
@@ -47,15 +51,14 @@ var questionList = [
             `D. /* This is a comment */`
         ],
         correctAnswer: `B. // This is a comment`
-    },
+    }
 ];
-
 
 
 // start timer
 function startTimer() {
-    timeCount = 90
-    startButton.disabled = true;
+    timeCount = 100;
+    // startButton.disabled = true;
     startQuiz();
 
 }
@@ -64,31 +67,28 @@ startButton.addEventListener("click", startTimer);
 // startagainButton.addEventListener("click", startQuiz)
 
 function startQuiz() {
+    headercontainerEl.remove();
     displayQuestion();
     //set timers
     timer = setInterval(function () {
         timeCount --;
         quiztimeEl.textContent = `You have ${timeCount} seconds left`;
-        startButton.textContent = `Good Luck`
         if (timeCount === 1) {
             quiztimeEl.textContent = `You have ${timeCount} second left`;
         }
-        else if (timeCount === 0) {
+        else if (timeCount <= 0) {
             clearInterval(timer);
             quiztimeEl.textContent = `Quiz Completed`;
-            startButton.hidden = true;
-            // startagainButton.textContent = `Try Again`;
+            displayResults();
         } 
     }, 1000)
 }
 
 // display the quiz question 
 function displayQuestion(){
-    var questionCount = 0;
-    var score = 0 ;
     if (questionCount < questionList.length) {
         questionEl.innerHTML = questionList[questionCount].question;
-        answerEl.textContent = " ";
+        answerEl.textContent = "";
         //get answers from array
         for (let i = 0; i < questionList[questionCount].answer.length; i++) {
             let element = document.createElement("button"); 
@@ -98,15 +98,17 @@ function displayQuestion(){
                 event.stopPropagation();
 
                 if (element.innerText === questionList[questionCount].correctAnswer) {
-                   score += timeCount;
+                   score += 30; //+ 30 points for correct answer
                 }
                 else {
-                    score -= 5;
-                    timeCount = timeCount - 10;
+                    score -= 15; // - 15 points for incorrect answer plus - 15 seconds to the timecount
+                    timeCount = timeCount - 15;
                 }
-                questionEl.innerHTML ="";
-                if (questionCount === questionList.length) {
-                    return;
+                // console.log(score);
+                // console.log(timeCount);
+                if (questionCount === questionList.length-1) {
+                    displayResults()
+                    quiztimeEl.remove();
                 } else {
                     questionCount++;
                     displayQuestion();
@@ -115,4 +117,21 @@ function displayQuestion(){
             answerEl.append(element);
         }
     }
+}
+//varibale for the array of hight scores from local
+var storeArrays = JSON.parse(window.localStorage.getItem("highScores"))
+var scoreEl = document.querySelector(".score");
+//display the results
+function displayResults() {
+    quizcontainerEl.remove();
+   
+    scoreEl.innerHTML = `Your score: ${score} points`;
+  
+
+}
+
+//reset the scores 
+var resetButton = document.createElement("reset-button");
+function resetScores() {
+    score = 0;
 }
